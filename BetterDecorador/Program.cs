177097523c39b400a495c827512b8acd4dcb7545
ivyycore <mariasequeira.dev@gmail.com>
2025-03-13 01:50:18 +0000

    Added method

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -4,9 +4,19 @@ namespace BetterDecorador
 {
     public class Program
     {
-        private static void Main(string[] args)
-        {
-            Console.WriteLine("Hello LP1!");
-        }
-    }
-}
+         private static void Main(string[] args)
+         {
+             Console.WriteLine(Decor(args[0], char.Parse(args[1]), int.Parse(args[2])));
+         }
+ 
+         private static string Decor (string phrase, char symbol, int numbers)
+         {
+             string realsymbol = " ";
+             for (int i = 0; i < numbers; i++)
+             {
+                 realsymbol += symbol;
+             }
+             return $"{realsymbol} {phrase} {realsymbol}";
+         }
+     }
+ }
