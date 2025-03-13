56b119e997cce2c05bf2e0365ffc38394293f056
ivyycore <mariasequeira.dev@gmail.com>
2025-03-13 15:07:30 +0000

    Added comments

--- Special/Program.cs
+++ Special/Program.cs
@@ -6,12 +6,21 @@ namespace Special
     {
         private static void Main(string[] args)
         {
+            /// <summary>
+            /// Asks the user a number and saves it on GetSpecial
+            /// <summary>
+            /// <param name="args"></param>
             Console.Write("Insert n-th number of the special sequence: ");
             int number = int.Parse(Console.ReadLine());
             Console.WriteLine(GetSpecial(number));
         }
         private static int GetSpecial(int n)
         {
+            /// <summary>
+            /// Checks the variable n and gets the next number on the sequence
+            /// </summary>
+            /// <param name="n">Number selected</param>
+            /// <returns> The next number on the sequence.</returns>
             int result;
             if (n==0) {return 0;}
             else if (n==1) {return 1;}
