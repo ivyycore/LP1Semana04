6db3f4fd69d9821ba5a12860b0f6098eaee630f7
ivyycore <mariasequeira.dev@gmail.com>
2025-03-13 14:47:59 +0000

    Added first method for number input

---
+++ Special/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Special
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Insert n-th number of the special sequence: ");
+            int number = int.Parse(Console.Readline());
+            Console.WriteLine(GetSpecial(number));
+        }
+    }
+}
