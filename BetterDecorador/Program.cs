09afb75c6c433f9f9613773ce1ebb34f8d11cf84
ivyycore <mariasequeira.dev@gmail.com>
2025-03-13 11:37:23 +0000

    Added comment to the method Decor

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -13,7 +13,14 @@ namespace BetterDecorador
          {
              Console.WriteLine(Decor(args[0], char.Parse(args[1]), int.Parse(args[2])));
          }
- 
+        /// <summary>
+        /// Creates a string that repeats the input given to the string, s
+        /// so that it can print it the right number of times
+        /// </summary>
+        /// <param name="phrase">The phrase given first</param>
+        /// <param name="symbol">The symbol given in second place</param>
+        /// <param name="numbers">The number of time the symbol will appear </param>
+        /// <returns>Prints at the end </returns>
          private static string Decor (string phrase, char symbol, int numbers)
          {
              string realsymbol = " ";
