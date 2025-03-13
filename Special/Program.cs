639aefe0ee8e3657c88df0a22a575bf71f7c40b5
ivyycore <mariasequeira.dev@gmail.com>
2025-03-13 15:00:28 +0000

    Made the GetSpecial method

--- Special/Program.cs
+++ Special/Program.cs
@@ -7,8 +7,17 @@ namespace Special
         private static void Main(string[] args)
         {
             Console.Write("Insert n-th number of the special sequence: ");
-            int number = int.Parse(Console.Readline());
+            int number = int.Parse(Console.ReadLine());
             Console.WriteLine(GetSpecial(number));
+        }
+        private static int GetSpecial(int n)
+        {
+            int result;
+            if (n==0) {return 0;}
+            else if (n==1) {return 1;}
+            else
+                result = GetSpecial(n-1)+2*GetSpecial(n-2);
+                return result;
         }
     }
 }
